0cdd476f4f648cb035ee26626ed71bd92085264b
Arthur Alef <arthur.arnascimento@gmail.com>
2024-01-16 23:37:42 -0300

    added shopping list page

---
+++ cypress/pages/shoppingListPage.js
@@ -0,0 +1,16 @@
+class shoppingList {
+
+    selectorsList() {
+        const selectors = {
+            homePageButton: "[data-testid='paginaInicial']",
+            addToCart: "[data-testid='adicionar carrinho']",
+            clearListButton: "[data-testid='limparLista']",
+            productDecreaseButton: "[data-testid='product-decrease-quantity']",
+            productIncreaseButton: "[data-testid='product-increase-quantity']"
+        }
+
+    }
+
+}
+
+export default shoppingList
