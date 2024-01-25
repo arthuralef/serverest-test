b88182f2ae266b8ee91127713e10eb0cb4a97159
Arthur Alef <arthur.arnascimento@gmail.com>
2024-01-24 22:51:37 -0300

    added new user fail

--- cypress/e2e/newAccountSuccess.spec.cy.js
+++ cypress/e2e/newAccountSuccess.spec.cy.js
@@ -1,11 +1,13 @@
 import login from "../pages/loginPage"
 import createAccount from "../pages/createAccountPage"
+import header from "../pages/headerPage"
 
 const Chance = require('chance')
 
 const chance = new Chance()
 const loginPage = new login()
 const createAccountPage = new createAccount()
+const headerPage = new header ()
 
 describe('Teste de novo usuario Serverest', () => {
     it('Criar usuario - Sucesso', () =>{
@@ -17,5 +19,18 @@ describe('Teste de novo usuario Serverest', () => {
         createAccountPage.createNewAccount(chance.first(), chance.email(), chance.cpf())
 
         createAccountPage.checkpage()
+
+        headerPage.logout()
+
+    })
+
+    it('Criar Usuario - Falha', () =>{
+
+        loginPage.checkpage()
+
+        loginPage.createNewAccount()
+
+        createAccountPage.createNewAccount(chance.first(), chance.cpf(), chance.cpf())
+        
     })
 })
